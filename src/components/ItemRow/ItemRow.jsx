ae7d67a9fed65b55f6f752e9d6275a64a370b1b5
Franco <f.galuzzi@gmail.com>
2021-02-09 11:34:29 -0300

    meli challenge dia 5: Icon free_shipping

--- src/components/ItemRow/ItemRow.jsx
+++ src/components/ItemRow/ItemRow.jsx
@@ -1,10 +1,11 @@
 import styles from "./ItemRow.module.css";
 import PropTypes from "prop-types";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShippingFast } from "@fortawesome/free-solid-svg-icons";
 import {formatNumber} from "../../utils/";
 
 const ItemRow = ({data}) => {
-    let {id, picture, price, title} = data;
+    let {id, picture, price, title, free_shipping} = data;
     let handleClick = () => {
         window.location = `/items/${id}`;
     };
@@ -12,7 +13,9 @@ const ItemRow = ({data}) => {
         <div className={styles["item-row"]} onClick={() => handleClick()}>
             <img className={styles["item-row-img"]} src={picture}/>
             <div className={styles["item-row-container"]}>
-                <h3 className={styles["item-row-price"]}>{price.currency === "ARS" ? "$" : "U$D"}  {formatNumber(price.amount)}</h3>
+                <h3 className={styles["item-row-price"]}>{price.currency === "ARS" ? "$" : "U$D"}  {formatNumber(price.amount)}
+                    {free_shipping && <FontAwesomeIcon className={styles["item-row-free-shipping"]} size="xs" icon={faShippingFast} />}
+                </h3>
                 <h3 className={styles["item-row-name"]}>{title}</h3>
             </div>
         </div>
